0a1d210e81232fbc7d2fb980f62fb4fec02d01eb
saashqdev <saashqdev@gmail.com>
2025-06-26 13:32:10 -0400

    add betterstack middleware

--- src/lib/asyncLocalStorage.ts
+++
@@ -1,6 +0,0 @@
-import type { ILogtailLog } from '@logtail/types'
-import { AsyncLocalStorage } from 'node:async_hooks'
-
-export const asyncLocalStorage = new AsyncLocalStorage<{
-  logger: ILogtailLog
-}>()
